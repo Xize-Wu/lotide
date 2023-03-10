b5c42af5fbbe2ddd513af65da3d382180a0b36ad
Xize Wu <wuxize1995@gmail.com>
2023-03-09 19:00:24 -0500

    initial commitment

---
+++ takeUntil.js
@@ -0,0 +1,47 @@
+const takeUntil = function(array, callback) {
+  let output = [];
+  for (let element of array) {
+    if (!callback(element)) {
+      output.push(element);
+    }
+    if (callback(element)) {
+      return output;
+    }
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (!eqArrays(actual, expected)) {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+
+assertEqual(results1,
+  [ 1, 2, 5, 7, 2 ]);
+
+assertEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+
